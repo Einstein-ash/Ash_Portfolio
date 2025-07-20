e5cc60edd46ccd6c216b689da65186f70896bdcd
Ashwani <ashwanisingh46572@gmail.com>
2025-07-20 12:26:26 +0530

    added resume 4

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // import CV from '../../assets/cv1.pdf'
-import CV from '../../assets/ash_cv.pdf'
+// import CV from '../../assets/ash_cv.pdf'
+import CV from '../../assets/Resume_Ashwani_FE_4.pdf'
 export const CTA = () => {
   return (
     <div className='cta'>
